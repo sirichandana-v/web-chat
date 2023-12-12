fd8f17f2921b9c3d1ef3bbcdd8b5e53223afcc15
Siri Chandana Vemaraju <svemaraju@seattleu.edu>
2023-12-12 09:51:14 -0800

    basic server setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, console.log(`server started ${PORT}`));
